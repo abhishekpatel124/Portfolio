70d1ab9ee6d90a77846d43670e103dba12b2f2fb
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-07 18:54:16 +0530

    started making it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import taiwind from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwind() ,react()],
+  plugins: [react()],
 })
